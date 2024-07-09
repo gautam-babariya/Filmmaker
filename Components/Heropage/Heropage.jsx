61dce48a20287b1630a120203d3529fc613e0ba2
gautam <gautambabariya0125@gmail.com>
2024-07-09 18:40:32 +0530

    cl

--- Components/Heropage/Heropage.jsx
+++ Components/Heropage/Heropage.jsx
@@ -17,7 +17,7 @@ function heropage() {
   var objectIdTofind = id;
   useEffect(() => {
     axios.get('https://filmmaker-api.vercel.app/getvideo')
-    // https://filmmaker-api.vercel.app/getvideo
+    // axios.get('http://localhost:5500/getvideo')
     .then(async (res) => setproductdetain(res.data)
     );
   }, []);
